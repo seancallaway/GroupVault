ee27ac481716fe0265031a5fbece7619d503abb8
Sean Callaway <seancallaway@gmail.com>
2020-05-08 11:20:24 -0700

    Added base template.

--- groupvault/settings.py
+++ groupvault/settings.py
@@ -39,7 +39,9 @@ ROOT_URLCONF = 'groupvault.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
