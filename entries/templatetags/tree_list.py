7e2950a6de0526d762c836654df43c29d5801591
Sean Callaway <Seancallaway@gmail.com>
2020-05-09 11:52:37 -0700

    Make folders visible in folder area (#19)
    * Added folder admin.
    
    * Display folders as nested lists on left.
    
    * Added missing ul tags.
    
    * Ensured extrascripts occurs after jquery.
    
    * Added folder icons.

---
+++ entries/templatetags/tree_list.py
@@ -0,0 +1,24 @@
+from django import template
+
+register = template.Library()
+
+
+def build_limb(folders):
+    output = ''
+    for folder in folders:
+        if not folder.sub_folders.all():
+            output += f'<li><i class="far fa-folder"></i> <a href="#">{folder.name}</a></li>\n'
+        else:
+            output += f'<li><i class="far fa-folder"></i> <a href="#">{folder.name}</a>\n<ul>\n'
+            output += build_limb(folder.sub_folders.all())
+            output += '</ul>\n</li>\n'
+
+    return output
+
+
+@register.filter(is_safe=True)
+def tree_list(value):
+    if value:
+        return '<ul>\n' + build_limb(value) + '</ul>\n'
+    else:
+        return '<p>There are no folders here.</p>\n<p>An admin needs to create at least one root folder.</p>'
