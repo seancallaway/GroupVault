404a8416b02853e66d9071c61aa9a1efce65fd5b
Sean Callaway <seancallaway@gmail.com>
2020-05-09 14:48:22 -0700

    Added Bootstrap version test.

--- entries/tests.py
+++ entries/tests.py
@@ -2,12 +2,16 @@ from django.test import TestCase
 from django.urls import resolve
 from entries.models import Entry, Folder
 from entries.views import HomePage
+from users.models import CustomUser
 
 
 BOOTSTRAP_VERSION = 'bootstrap/4.4.1'
 
 
 class HomePageTest(TestCase):
+
+    def setUp(self):
+        CustomUser.objects.create_user(username='testuser', email='test@fake.com', password='testpass')
 
     def test_root_url_resolves_to_home_page(self):
         found = resolve('/')
@@ -16,6 +20,11 @@ class HomePageTest(TestCase):
     def test_home_page_redirects_anonymous(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 302)
+
+    def test_home_page_uses_correct_bootstrap(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get('/')
+        self.assertContains(response, BOOTSTRAP_VERSION)
 
 
 class FolderTest(TestCase):
