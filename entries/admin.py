efacd3b8dd938e6b2a22c11381e8f919d091de00
Sean Callaway <seancallaway@gmail.com>
2020-05-11 13:38:33 -0700

    Allow Entry creation from admin.

--- entries/admin.py
+++ entries/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from entries.forms import EntryAdminChangeForm
+from entries.forms import EntryAdminAddForm, EntryAdminChangeForm
 from entries.models import Entry, Folder
 
 
 class EntryAdmin(admin.ModelAdmin):
-    form = EntryAdminChangeForm
     model = Entry
     list_display = ['name', 'folder', ]
     list_filter = ('folder__name', )
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        if change:
+            kwargs['form'] = EntryAdminChangeForm
+        else:
+            kwargs['form'] = EntryAdminAddForm
+        return super().get_form(request, obj, change, **kwargs)
 
 
 class FolderAdmin(admin.ModelAdmin):
