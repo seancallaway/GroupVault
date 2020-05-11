56714e2268259bc6106f41119a77d96b74d78ac5
Sean Callaway <Seancallaway@gmail.com>
2020-05-11 13:42:27 -0700

    Add Entry to Admin (#28)
    * Set proper plural for Entry.
    
    * Added basic Entry admin entry.
    
    * Allow filtering by Folder.
    
    * Allow Entry creation from admin.

--- entries/admin.py
+++ entries/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from entries.models import Folder
+from entries.forms import EntryAdminAddForm, EntryAdminChangeForm
+from entries.models import Entry, Folder
+
+
+class EntryAdmin(admin.ModelAdmin):
+    model = Entry
+    list_display = ['name', 'folder', ]
+    list_filter = ('folder__name', )
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        if change:
+            kwargs['form'] = EntryAdminChangeForm
+        else:
+            kwargs['form'] = EntryAdminAddForm
+        return super().get_form(request, obj, change, **kwargs)
 
 
 class FolderAdmin(admin.ModelAdmin):
@@ -9,4 +23,5 @@ class FolderAdmin(admin.ModelAdmin):
     ]
 
 
+admin.site.register(Entry, EntryAdmin)
 admin.site.register(Folder, FolderAdmin)
--- entries/forms.py
+++ entries/forms.py
@@ -2,6 +2,39 @@ from django import forms
 from entries.models import Entry
 
 
+class EntryAdminAddForm(forms.ModelForm):
+    secret = forms.CharField(max_length=512)
+
+    def save(self, commit=True):
+        entry_instance = super(EntryAdminAddForm, self).save(commit=False)
+        entry_instance.secret = self.cleaned_data['secret']
+        entry_instance.save()
+        return entry_instance
+
+    class Meta:
+        model = Entry
+        fields = (
+            'name',
+            'folder',
+            'url',
+            'login',
+            'secret',
+            'description',
+        )
+
+
+class EntryAdminChangeForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = (
+            'name',
+            'folder',
+            'url',
+            'login',
+            'description',
+        )
+
+
 class EntryForm(forms.ModelForm):
     secret = forms.CharField(max_length=512)
 
