2aff12e9fc810e921e4e3e182ab44baedfe5e075
Sean Callaway <seancallaway@gmail.com>
2020-05-11 13:11:41 -0700

    Added basic Entry admin entry.

--- entries/admin.py
+++ entries/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from entries.models import Folder
+from entries.forms import EntryAdminChangeForm
+from entries.models import Entry, Folder
+
+
+class EntryAdmin(admin.ModelAdmin):
+    form = EntryAdminChangeForm
+    model = Entry
+    list_display = ['name', 'folder', ]
 
 
 class FolderAdmin(admin.ModelAdmin):
@@ -9,4 +16,5 @@ class FolderAdmin(admin.ModelAdmin):
     ]
 
 
+admin.site.register(Entry, EntryAdmin)
 admin.site.register(Folder, FolderAdmin)
--- entries/forms.py
+++ entries/forms.py
@@ -2,6 +2,18 @@ from django import forms
 from entries.models import Entry
 
 
+class EntryAdminChangeForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = (
+            'name',
+            'folder',
+            'url',
+            'login',
+            'description',
+        )
+
+
 class EntryForm(forms.ModelForm):
     secret = forms.CharField(max_length=512)
 
